6bd6cf53ca6d1acf28000d0daab35f1c4b1a80ac
lifeinline1 <lifeinline1@gmail.com>
2024-04-18 17:14:05 +0200

    first commit

---
+++ src/main/java/com/jobapp/companyms/company/Company.java
@@ -0,0 +1,50 @@
+package com.jobapp.companyms.company;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+
+    public Company() {
+    }
+    public Company(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
+
---
+++ src/main/java/com/jobapp/companyms/company/CompanyService.java
@@ -0,0 +1,13 @@
+package com.jobapp.companyms.company;
+
+import java.util.List;
+
+public interface CompanyService {
+
+    List<Company> getAllCompanies();
+    Company getCompanyById(Long id);
+    String addCompany(Company company);
+    String updateCompany(Long id, Company company);
+    String deleteCompany(Long id);
+
+}
